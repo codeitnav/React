767098622b326c5355dce7a67778ed3a33c55359
codeitnav <navya.sriv.1110@gmail.com>
2024-10-15 19:07:36 +0530

    update files

--- 13megaBlog/src/App.jsx
+++ 13megaBlog/src/App.jsx
@@ -5,12 +5,14 @@ import authService from './appwrite/auth'
 import { login, logout } from './store/authSlice'
 import Header from './components/header/Header';
 import Footer from './components/Footer/Footer';
-
+import { useNavigate } from 'react-router-dom';
 function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   // since we need to see whether we're connected to Appwrite or some other network, so it req some loading time
   // if loading = true show loading icon otherwise show data
   const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     authService.getCurrentUser()
@@ -18,11 +20,16 @@ function App() {
       if(userData){
         dispatch(login({userData}))
       } else {
+        navigate('/signup');
         dispatch(logout())
       }
     })
     .finally(() => setLoading(false))
   }, [])
+  
+  const handleSignupClick = () => {
+    navigate('/signup');
+  }
 
   return !loading ? (
     <div className='min-h-screen flex flex-wrap content-between bg-rose-100'>
@@ -32,6 +39,9 @@ function App() {
           
         </main>
         <Footer />
+        {!authService.getCurrentUser() && (
+          <button onClick={handleSignupClick}>Signup</button>
+        )}
       </div>
     </div>
   ) : null
